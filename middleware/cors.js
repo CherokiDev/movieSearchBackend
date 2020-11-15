fc4593e7dd2a617e2c7caa58bd3d63c62260aec8
David <d.pitarch@outlook.es>
2020-11-15 22:57:18 +0100

    Edit CORS.js

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,5 +1,4 @@
 module.exports = (req, res, next) => {
-    console.log("putos cors")
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-API-KEY, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE");
